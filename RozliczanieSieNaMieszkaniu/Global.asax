391ef24937ac88d35394f2b82ed7bacb91257e21
Maciek Klimowski <maciek.klimowski44@gmail.com>
2017-05-01 12:06:32 +0200

    Add project files.

---
+++ RozliczanieSieNaMieszkaniu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RozliczanieSieNaMieszkaniu.MvcApplication" Language="C#" %>
---
+++ RozliczanieSieNaMieszkaniu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RozliczanieSieNaMieszkaniu.Startup))]
+namespace RozliczanieSieNaMieszkaniu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
